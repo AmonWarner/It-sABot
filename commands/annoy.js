28541a71778a9481a52acd64cdf414d11b62d41c
SurviveThePersonPerson <38363905+SurviveThePersonPerson@users.noreply.github.com>
2018-12-08 15:22:30 -0500

    Create annoy.js

---
+++ commands/annoy.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let theUser = message.author;
+    message.delete();
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    message.channel.send("Hi I'm annoying");
+    return message.channel.send(`Oh and ${theUser} told me to say this soz.`);
+    }
+
+module.exports.help = {
+    name: "annoy"
+}
